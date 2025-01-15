d665f488abef6d7159ad510c9907d2f86e36e43c
Matheus Amorim <matheusnavesamorim@gmail.com>
2025-01-15 14:49:21 -0300

    Feat: updating and deleting books

--- back-end/app/Http/Controllers/livrosController.php
+++ back-end/app/Http/Controllers/livrosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Livros;
+use Exception;
 use Illuminate\Http\Request;
 
 class livrosController extends Controller
@@ -13,6 +14,43 @@ class livrosController extends Controller
     }
     public function store(Request $request)
     {
-        return response()->json(Livros::create($request->all()), status: 201);
+        try{
+            $retorno = Livros::create($request->all());
+            return response()->json($retorno, status: 201);
+        } catch(Exception $e){
+            return response()->json(['error' => true, 'message' => $e->getMessage()]);
+        }
+    }
+
+    public function show(int $livros)
+    {
+        $livrosModel = Livros::find($livros);
+        if ($livrosModel === null) {
+            return response()->json(['error' => true, 'message' => 'Livro não encontrado'], status: 404);
+        }
+        return $livrosModel;
+    }
+
+    public function update(Request $request, int $livros)
+    {
+        try{
+            Livros::where('id', $livros)->update($request->all());
+            return response()->json(Livros::find($livros), status: 201);
+        } catch(Exception $e){
+            return response()->json(['error' => true, 'message' => $e->getMessage()]);
+        }
+    }
+    public function destroy(int $livros)
+    {
+        try{
+            if (Livros::find($livros) !== null) {
+                Livros::destroy($livros);
+                return response()->json(['message' => 'Livro excluído com sucesso!'], status: 202);
+            } else {
+                return response()->json(['error' => true, 'message'=> 'Livro não encontrado'], status:404);
+            }
+        } catch (Exception $e){
+            return response()->json(['error' => true, 'message' => $e->getMessage()]);
+        }
     }
 }
--- back-end/app/Http/Controllers/usuariosController.php
+++ back-end/app/Http/Controllers/usuariosController.php
@@ -7,10 +7,6 @@ use Illuminate\Http\Request;
 
 class usuariosController extends Controller
 {
-    public function __construct(Usuarios $usuarios)
-    {
-    }
-
     public function index()
     {
         return Usuarios::all();
@@ -25,7 +21,7 @@ class usuariosController extends Controller
     {
         $usuariosModel = Usuarios::find($usuarios);
         if ($usuariosModel === null) {
-            return response()->json(['message' => 'Usuário não encontrado'], status: 404);
+            return response()->json(['error' => true, 'message' => 'Usuário não encontrado'], status: 404);
         }
         return $usuariosModel;
     }
@@ -35,7 +31,7 @@ class usuariosController extends Controller
         Usuarios::where('id', $usuarios)->update($request->all());
         return Usuarios::find($usuarios);
     }
-    
+
     public function destroy(int $usuarios)
     {
         usuarios::destroy($usuarios);
