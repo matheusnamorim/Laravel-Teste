ba9b06f433bb79e833cc3af0b14ed169c7bdec75
Matheus Amorim <matheusnavesamorim@gmail.com>
2025-01-15 14:29:21 -0300

    Feat: updating and deleting users

--- back-end/app/Http/Controllers/usuariosController.php
+++ back-end/app/Http/Controllers/usuariosController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class usuariosController extends Controller
 {
+    public function __construct(Usuarios $usuarios)
+    {
+    }
+
     public function index()
     {
         return Usuarios::all();
@@ -15,5 +19,26 @@ class usuariosController extends Controller
     public function store(Request $request)
     {
         return response()->json(Usuarios::create($request->all()), status: 201);
+    }
+
+    public function show(int $usuarios)
+    {
+        $usuariosModel = Usuarios::find($usuarios);
+        if ($usuariosModel === null) {
+            return response()->json(['message' => 'Usuário não encontrado'], status: 404);
+        }
+        return $usuariosModel;
+    }
+
+    public function update(Request $request, int $usuarios)
+    {
+        Usuarios::where('id', $usuarios)->update($request->all());
+        return Usuarios::find($usuarios);
+    }
+    
+    public function destroy(int $usuarios)
+    {
+        usuarios::destroy($usuarios);
+        return response()->noContent();
     }
 }
