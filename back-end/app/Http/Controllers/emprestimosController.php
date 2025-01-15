40421e04c7ae35bee130f59e3eb1e33be36f4c8f
Matheus Amorim <matheusnavesamorim@gmail.com>
2025-01-15 16:05:09 -0300

    Feat: updating and deleting loan

--- back-end/app/Http/Controllers/emprestimosController.php
+++ back-end/app/Http/Controllers/emprestimosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Emprestimos;
+use Exception;
 use Illuminate\Http\Request;
 
 class emprestimosController extends Controller
@@ -14,6 +15,44 @@ class emprestimosController extends Controller
 
     public function store(Request $request)
     {
-        return response()->json(Emprestimos::create($request->all()), status: 201);
+        try{
+            $retorno = Emprestimos::create($request->all());
+            return response()->json($retorno, status: 201);
+        } catch(Exception $e){
+            return response()->json(['error' => true, 'message' => $e->getMessage()]);
+        }
+    }
+
+    public function show(int $emprestimmos)
+    {
+        $emprestimmosModel = Emprestimos::find($emprestimmos);
+        if ($emprestimmosModel === null) {
+            return response()->json(['error' => true, 'message' => 'Empréstimo não encontrado'], status: 404);
+        }
+        return $emprestimmosModel;
+    }
+
+    public function update(Request $request, int $emprestimmos)
+    {
+        try{
+            Emprestimos::where('id', $emprestimmos)->update($request->all());
+            return response()->json(Emprestimos::find($emprestimmos), status: 201);
+        } catch(Exception $e){
+            return response()->json(['error' => true, 'message' => $e->getMessage()]);
+        }
+    }
+
+    public function destroy(int $emprestimmos)
+    {
+        try{
+            if (Emprestimos::find($emprestimmos) !== null) {
+                Emprestimos::destroy($emprestimmos);
+                return response()->json(['message' => 'Empréstimo excluído com sucesso!'], status: 202);
+            } else {
+                return response()->json(['error' => true, 'message'=> 'Empréstimo não encontrado'], status:404);
+            }
+        } catch (Exception $e){
+            return response()->json(['error' => true, 'message' => $e->getMessage()]);
+        }
     }
 }
