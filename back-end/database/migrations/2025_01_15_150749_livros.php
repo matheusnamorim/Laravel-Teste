581bd9a8682f63c1cd4c31b589426761ae0e2cec
Matheus Amorim <matheusnavesamorim@gmail.com>
2025-01-15 12:13:13 -0300

    Feat: migrations

---
+++ back-end/database/migrations/2025_01_15_150749_livros.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livros', function (Blueprint $table) {
+            $table->id();
+            $table->string(column: 'titulo')->nullable(false);
+            $table->unsignedBigInteger('autor_id')->nullable(false);
+            $table->foreign('autor_id')->on('usuarios')->references('id');
+            $table->enum('situacao', ['Emprestado', 'Disponivel'])->default('Disponivel');
+            $table->string(column: 'genero')->nullable(false);
+            $table->string(column: 'sinopse')->nullable(false);
+            $table->year(column: 'ano_publicacao')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livros');
+    }
+};
